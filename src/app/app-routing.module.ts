a74a2308dd3a59614cad32fbef458b99da55c2db
jessamynsmith <jessamyn.smith@gmail.com>
2019-10-04 20:10:27 -0600

    Don't need top level urls for child tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  { path: 'tab1', loadChildren: './tab1/tab1.module#Tab1PageModule' },
-  { path: 'tab2', loadChildren: './tab2/tab2.module#Tab2PageModule' },
-  { path: 'tab3', loadChildren: './tab3/tab3.module#Tab3PageModule' },
+  // e.g. http://localhost:8100/tabs/tabs/tab1
   { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
 ];
 
